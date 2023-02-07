02719cdbec7166ba0e38c509a52b70e24b73b773
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-02-07 20:21:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import { ColorModeContext, useMode } from "./Theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 
 function App() {
-  const { theme, colorMode } = useMode();
-  return <div className="app"></div>;
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+
+        <div className="app">
+          <main className="content"></main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
+
+// cssBaseline resert css styles to defaults
