9a501eb5bede03293f758566cfecb648111f682f
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-02-07 20:00:25 +0100

    fix colors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+import { ColorModeContext, useMode } from "./Theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
 function App() {
+  const { theme, colorMode } = useMode();
   return <div className="app"></div>;
 }
 
