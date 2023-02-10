b181f195c2efebfa0a2780d7acdf72e03c70dadf
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-02-10 21:47:54 +0100

    geography

--- src/scence/dashboard/Geogeraphy.jsx
+++ src/scence/dashboard/Geogeraphy.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import GeographyChart from "../../components/GeographyChart";
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
 const Geogeraphy = () => {
-  return <div>Geogeraphy</div>;
+  return (
+    <Box m="20px">
+      <Header title="Line Chart" subtitle="Simple Line Chart" />
+      <Box height="75vh">
+        <GeographyChart />
+      </Box>
+    </Box>
+  );
 };
 
 export default Geogeraphy;
