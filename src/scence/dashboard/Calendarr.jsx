c60550ad627fd3b1779bc4cc7281f1f3d97419de
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-02-10 01:15:40 +0100

    Calendar

--- src/scence/dashboard/Calendarr.jsx
+++ src/scence/dashboard/Calendarr.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import Calendar, { formatDate } from "@fullcalendar/core";
+import { useState } from "react";
+import FullCalendar from "@fullcalendar/react";
+import { formatDate } from "@fullcalendar/core";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
@@ -15,19 +16,19 @@ import {
 import Header from "../../components/Header";
 import { tokens } from "../../Theme";
 
-const Calendarr = () => {
+const Calendar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const [currentEvents, setCurrentEvents] = useState([]);
 
-  const handelDataClick = (selected) => {
-    const title = prompt("please enter new title for your event");
+  const handleDateClick = (selected) => {
+    const title = prompt("Please enter a new title for your event");
     const calendarApi = selected.view.calendar;
     calendarApi.unselect();
 
     if (title) {
       calendarApi.addEvent({
-        id: `${selected.dataStr}-${title}`,
+        id: `${selected.dateStr}-${title}`,
         title,
         start: selected.startStr,
         end: selected.endStr,
@@ -36,20 +37,22 @@ const Calendarr = () => {
     }
   };
 
-  const handelEventClicked = (selected) => {
+  const handleEventClick = (selected) => {
     if (
       window.confirm(
-        `are yo sure ypu want to delete the event${selected.event.title}`
+        `Are you sure you want to delete the event '${selected.event.title}'`
       )
     ) {
       selected.event.remove();
     }
   };
+
   return (
     <Box m="20px">
-      <Header title="Calendar" subtitle="Full Calendar InterActive Page" />
+      <Header title="Calendar" subtitle="Full Calendar Interactive Page" />
+
       <Box display="flex" justifyContent="space-between">
-        {/* Calender sider bar */}
+        {/* CALENDAR SIDEBAR */}
         <Box
           flex="1 1 20%"
           backgroundColor={colors.primary[400]}
@@ -62,7 +65,7 @@ const Calendarr = () => {
               <ListItem
                 key={event.id}
                 sx={{
-                  backgroundColors: colors.greenAccent[500],
+                  backgroundColor: colors.greenAccent[500],
                   margin: "10px 0",
                   borderRadius: "2px",
                 }}
@@ -71,10 +74,11 @@ const Calendarr = () => {
                   primary={event.title}
                   secondary={
                     <Typography>
-                      {
-                        (formatDate(event.start),
-                        { year: "numeric", month: "short", day: "numeric" })
-                      }
+                      {formatDate(event.start, {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                      })}
                     </Typography>
                   }
                 />
@@ -82,9 +86,47 @@ const Calendarr = () => {
             ))}
           </List>
         </Box>
+
+        {/* CALENDAR */}
+        <Box flex="1 1 100%" ml="15px">
+          <FullCalendar
+            height="75vh"
+            plugins={[
+              dayGridPlugin,
+              timeGridPlugin,
+              interactionPlugin,
+              listPlugin,
+            ]}
+            headerToolbar={{
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
+            }}
+            initialView="dayGridMonth"
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+            select={handleDateClick}
+            eventClick={handleEventClick}
+            eventsSet={(events) => setCurrentEvents(events)}
+            initialEvents={[
+              {
+                id: "12315",
+                title: "All-day event",
+                date: "2022-09-14",
+              },
+              {
+                id: "5123",
+                title: "Timed event",
+                date: "2022-09-28",
+              },
+            ]}
+          />
+        </Box>
       </Box>
     </Box>
   );
 };
 
-export default Calendarr;
+export default Calendar;
