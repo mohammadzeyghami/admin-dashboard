bbc6757f57441b9d9d9b26d12d924d4319099649
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-02-10 10:15:32 +0100

    Update FAQ.jsx

--- src/scence/dashboard/FAQ.jsx
+++ src/scence/dashboard/FAQ.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { Box, useTheme, Typography } from "@mui/material";
+import Header from "../../components/Header";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { tokens } from "../../Theme";
 const FAQ = () => {
-  return <div>FAQ</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Box m="20px">
+      <Header title="FAQ" subtitle="frequently Asked Questions Page" />
+    </Box>
+  );
 };
 
 export default FAQ;
