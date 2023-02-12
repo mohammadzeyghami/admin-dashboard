7123627bf6678c1315de1aa87c50c20f06dc3196
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-02-12 19:14:14 +0100

    Update Topbar.jsx

--- src/scence/global/Topbar.jsx
+++ src/scence/global/Topbar.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../Theme";
+import InputBase from "@mui/material";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotifitactionOutlinedIcon from "@mui/icons-material/NotifitactionOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/SearchOutlined";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 const Topbar = () => {
   return <div>Topbar</div>;
 };
